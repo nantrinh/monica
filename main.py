c593f66c8565bdc9cfcf7af733101b5733bf4e3e
Nancy Trinh <nancytrinh20@gmail.com>
2019-11-25 18:11:48 -0800

    [REFACTOR] classes

--- main.py
+++ main.py
@@ -1,40 +1,65 @@
 import json
 import pandas as pd
 import glob
+from tqdm import tqdm
 
 
-def parse_chat(chat):
-    row = {}
-    row['engagementId'] = chat['info']['engagementId']
-    row['startTime'] = chat['info']['startTime']
-    row['endTime'] = chat['info']['endTime']
-    row['visitorId'] = chat['info']['visitorId']
-    row['visitorName'] = chat['info']['visitorName']
-    row['agentLoginName'] = chat['info']['agentLoginName']
-    row['agentFullName'] = chat['info']['agentFullName']
-    row['skillName'] = chat['info']['skillName']
+class Chat:
+    def __init__(self, json):
+        self.json = json
 
-    row['text'] = []
-    row['by'] = []
-    row['source'] = []
+    def to_csv(self, filename):
+      if filename[-4::] != '.csv':
+          filename = filename + '.csv'
+      df = self.__df()
+      df.to_csv(f'{filename}', index=False)
 
-    for line in chat['transcript']['lines']:
-        row['text'].append(line['text'])
-        row['by'].append(line['by'])
-        row['source'].append(line['source'])
+    def __df(self):
+      row = {}
+      row['engagementId'] = self.json['info']['engagementId']
+      row['startTime'] = self.json['info']['startTime']
+      row['endTime'] = self.json['info']['endTime']
+      row['visitorId'] = self.json['info']['visitorId']
+      row['visitorName'] = self.json['info']['visitorName']
+      row['agentLoginName'] = self.json['info']['agentLoginName']
+      row['agentFullName'] = self.json['info']['agentFullName']
+      row['skillName'] = self.json['info']['skillName']
+  
+      row['text'] = []
+      row['by'] = []
+      row['source'] = []
+  
+      for line in self.json['transcript']['lines']:
+          row['text'].append(line['text'])
+          row['by'].append(line['by'])
+          row['source'].append(line['source'])
+  
+      return pd.DataFrame(row)
 
-    df = pd.DataFrame(row)
-    return df
 
+class ChatFiles:
+    def __init__(self, **kwargs):
+        self.input_dir = kwargs.get('input_dir', 'input')
+        self.input_filetype = kwargs.get('input_filetype', 'txt')
+        self.output_dir = kwargs.get('output_dir', 'output')
 
-def json_to_df(data, name):
-    parsed = parse_chat(data)
-    parsed.to_csv(name + '.csv', index=False)
+        self.input_filenames = glob.glob(f'{self.input_dir}/chat_*.{self.input_filetype}')
 
+    def to_csv(self):
+      for input_filename in tqdm(self.input_filenames):
+        with open(input_filename, 'r') as read_file:
+            chat = Chat(json.load(read_file))
+            chat.to_csv(self.__output_filename(input_filename))
+
+    def __output_filename(self, input_filename):
+        start_index = len(self.input_dir) + 1
+        end_index = -(len(self.input_filetype) + 1)
+        name = input_filename[start_index : end_index]
+        return f'{self.output_dir}/{name}.csv'
+    
 
 if __name__ == '__main__':
-    filenames = glob.glob('chat_*.txt')
-    for filename in filenames:
-      with open(filename, 'r') as read_file:
-          data = json.load(read_file)
-          json_to_df(data, filename[:-4])
+    chatfiles = ChatFiles(input_dir='input',
+                          input_filetype='txt',
+                          output_dir='output',)
+    chatfiles.to_csv()
