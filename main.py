6e53178061a099b7e77482913344b894a7114db2
Nancy Trinh <nancytrinh20@gmail.com>
2019-11-26 16:31:24 -0800

    [ADD]: output only one csv

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ class Chat:
     def to_csv(self, filename):
       if filename[-4::] != '.csv':
           filename = filename + '.csv'
-      df = self.__df()
-      df.to_csv(f'{filename}', index=False)
+      df = self.to_df()
+      df.to_csv(filename, index=False)
 
-    def __df(self):
+    def to_df(self):
       row = {}
       row['engagementId'] = self.json['info']['engagementId']
       row['startTime'] = self.json['info']['startTime']
@@ -42,14 +42,27 @@ class ChatFiles:
         self.input_dir = kwargs.get('input_dir', 'input')
         self.input_filetype = kwargs.get('input_filetype', 'txt')
         self.output_dir = kwargs.get('output_dir', 'output')
+        self.output_filename = kwargs.get('output_filename', 'output.csv')
 
         self.input_filenames = glob.glob(f'{self.input_dir}/chat_*.{self.input_filetype}')
+        self.df = None 
 
-    def to_csv(self):
+    def to_csv(self, aggregate=True):
       for input_filename in tqdm(self.input_filenames):
         with open(input_filename, 'r') as read_file:
             chat = Chat(json.load(read_file))
-            chat.to_csv(self.__output_filename(input_filename))
+            if aggregate:
+                self.__compile_chats(chat.to_df())
+            else:
+                chat.to_csv(self.__output_filename(input_filename))
+      if aggregate:
+          self.df.to_csv(self.output_filename, index=False)
+
+    def __compile_chats(self, chat_df):
+        if self.df is None:
+            self.df = chat_df
+        else:
+            self.df = self.df.append(chat_df, ignore_index=True)
 
     def __output_filename(self, input_filename):
         start_index = len(self.input_dir) + 1
